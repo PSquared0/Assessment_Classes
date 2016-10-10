6b3bd4a80f8e769c253b187de87eed1c12a361d6
Pascaleee <pascaleee@gmail.com>
2016-10-09 19:12:37 -0700

    added files

---
+++ part4.py
@@ -0,0 +1,21 @@
+from assessment import Exam
+from assessment import Student
+from assessment import Question
+
+def take_test(exam, student):
+    score =  exam.admin()
+    student.score = score
+
+    return score
+
+def example():
+
+    exam = Exam()
+
+    exam.add_question("What are the basic Concepts of OOPs?","Abstraction,Encapsulation, Inheritance, Polymorphism.") 
+
+    exam.add_question("What is a blueprint for a software object is called?","A Class")
+    
+    exam.add_question("What is an object","They are instance of classes")   
+
+take_test(exam, student)
